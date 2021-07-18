343b2b335668c22b39b10471fe0cb0ec79f25313
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-18 18:55:26 -0500

    Create chromedriver_layer.sh

---
+++ chromedriver_layer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd .\chromedriver_layer
+wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip -OutFile .\chromedriver.zip
+wget https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-54/stable-headless-chromium-amazonlinux-2017-03.zip -OutFile .\headless-chromium.zip
+Expand-Archive .\headless-chromium.zip
+rm *.zip
