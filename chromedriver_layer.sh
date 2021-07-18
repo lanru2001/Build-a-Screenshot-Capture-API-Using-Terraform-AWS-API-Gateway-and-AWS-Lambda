dcc4c268bfdfc5e5c8201f6da6f95cc186a4cb85
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-18 18:58:01 -0500

    Update chromedriver_layer.sh

--- chromedriver_layer.sh
+++ chromedriver_layer.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-mkdir chromedriver_layer
-cd chromedriver_layer/
-wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip -OutFile chromedriver.zip
-wget https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-54/stable-headless-chromium-amazonlinux-2017-03.zip -OutFile headless-chromium.zip
-Expand-Archive headless-chromium.zip
+cd .\chromedriver_layer
+wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip -OutFile .\chromedriver.zip
+wget https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-54/stable-headless-chromium-amazonlinux-2017-03.zip -OutFile .\headless-chromium.zip
+Expand-Archive .\headless-chromium.zip
 rm *.zip
+
+cd ..\
+Compress-Archive .\chromedriver_layer -DestinationPath \chromedriver_layer.zip
