63c07119b02c16ba7a882cbaeb3977ed2c087ad0
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-18 19:34:38 -0500

    Create output.tf

---
+++ aws/output.tf
@@ -0,0 +1,6 @@
+output "api_gateway_url" {
+  value = "${aws_api_gateway_stage.dev_stage.invoke_url}/${aws_api_gateway_resource.screenshot_api_gateway.path_part}"
+}
+
+output "api_key" {
+  value = aws_api_gateway_api_key.apigw_dev_key.value
