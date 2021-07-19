97182c1a7e1bb5ca2a08be78911385f23411e9fe
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-18 19:30:15 -0500

    Update iam.tf

--- aws/iam.tf
+++ aws/iam.tf
@@ -68,3 +68,50 @@ resource "aws_iam_role_policy" "lambda_s3_access" {
 }
 EOF
 }
+
+resource "aws_iam_role" "apigw_cloudwatch" {
+  # https://gist.github.com/edonosotti/6e826a70c2712d024b730f61d8b8edfc
+  name = "api_gateway_cloudwatch_global"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "apigw_cloudwatch" {
+  name = "default"
+  role = aws_iam_role.apigw_cloudwatch.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents",
+                "logs:GetLogEvents",
+                "logs:FilterLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
