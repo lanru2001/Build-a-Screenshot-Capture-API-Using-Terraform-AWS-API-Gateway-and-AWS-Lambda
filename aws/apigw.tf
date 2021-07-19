3aef5930253a08e91ab2373e676a23a46c546f53
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-18 19:15:34 -0500

    Update apigw.tf

--- aws/apigw.tf
+++ aws/apigw.tf
@@ -1,3 +1,5 @@
+#This API will be used to direct all requests that are made for the screenshot service
+
 resource "aws_api_gateway_rest_api" "screenshot_api" {
   name        = "screenshot_api"
   description = "Lambda-powered screenshot API"
