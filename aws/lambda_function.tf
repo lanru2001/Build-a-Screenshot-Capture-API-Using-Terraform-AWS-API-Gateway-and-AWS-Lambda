629284e5a30e8335eac6f87db82bcec5342dd343
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-18 19:03:23 -0500

    Create lambda_function.tf

---
+++ aws/lambda_function.tf
@@ -0,0 +1,18 @@
+resource "aws_lambda_function" "take_screenshot" {
+  filename      = "./screenshot-service.zip"
+  function_name = "take_screenshot"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "screenshot-service.handler"
+  runtime       = "python3.7"
+
+  source_code_hash = filebase64sha256("./screenshot-service.zip")
+  timeout          = 600
+  memory_size      = 512 
+  layers = ["${aws_lambda_layer_version.chromedriver_layer.arn}"]
+
+  environment {
+    variables = {
+      s3_bucket = "${aws_s3_bucket.screenshot_bucket.bucket}"
+    }
+  }
+}
