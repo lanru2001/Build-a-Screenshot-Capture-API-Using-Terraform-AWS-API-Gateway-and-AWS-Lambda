3fa4736ddce4490e22251535401e035370b49986
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-18 19:26:59 -0500

    Update apigw.tf

--- aws/apigw.tf
+++ aws/apigw.tf
@@ -74,4 +74,29 @@ resource "aws_lambda_permission" "apigw" {
   source_arn = "${aws_api_gateway_rest_api.screenshot_api.execution_arn}/*/*/*"
 }
 
+#Set up API Gateway integration with the lambda function
+resource "aws_api_gateway_integration" "lambda_integration_get" {
+  depends_on = [
+    aws_lambda_permission.apigw
+  ]
+  rest_api_id = aws_api_gateway_rest_api.screenshot_api.id
+  resource_id = aws_api_gateway_method.take_screenshot_get.resource_id
+  http_method = aws_api_gateway_method.take_screenshot_get.http_method
 
+  integration_http_method = "POST" # https://github.com/hashicorp/terraform/issues/9271 Lambda requires POST as the integration type
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.take_screenshot.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "lambda_integration_post" {
+  depends_on = [
+    aws_lambda_permission.apigw
+  ]
+  rest_api_id = aws_api_gateway_rest_api.screenshot_api.id
+  resource_id = aws_api_gateway_method.take_screenshot_post.resource_id
+  http_method = aws_api_gateway_method.take_screenshot_post.http_method
+
+  integration_http_method = "POST" # https://github.com/hashicorp/terraform/issues/9271 Lambda requires POST as the integration type
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.take_screenshot.invoke_arn
+}
