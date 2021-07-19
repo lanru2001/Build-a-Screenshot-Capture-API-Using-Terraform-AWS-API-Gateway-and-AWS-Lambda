490434acda93c394dda8c92bb9163cdd0a9a6d09
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-18 19:02:18 -0500

    Update iam.tf

--- aws/iam.tf
+++ aws/iam.tf
@@ -18,3 +18,53 @@ resource "aws_iam_role" "lambda_exec_role" {
 }
 EOF
 }
+
+
+resource "aws_iam_role_policy" "lambda_logging" {
+  name = "lambda_logging"
+
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = <<EOF
+{
+    "Version"  : "2012-10-17",
+    "Statement": [
+        {
+            "Effect"  : "Allow",
+            "Resource": "*",
+            "Action"  : [
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+                "logs:CreateLogGroup"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+#lambda S3 access policy
+resource "aws_iam_role_policy" "lambda_s3_access" {
+  name = "lambda_s3_access"
+
+  role = aws_iam_role.lambda_exec_role.id
+
+  # TODO: Change resource to be more restrictive
+  policy = <<EOF
+{
+  "Version"  : "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:ListBuckets",
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+        "s3:GetObjectAcl"
+      ],
+      "Resource": ["*"]
+    }
+  ]
+}
+EOF
+}
